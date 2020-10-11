2ccec9a15bdfd0fab06a379a1596eb3ddc8e8749
Sergey K <89184083452.mail.ru>
2020-10-11 03:34:16 +0300

    Задача №1 - Заказ доставки карты

---
+++ README.md
@@ -0,0 +1,3 @@
+# homework_04Automated-testing_03-01
+
+[![Build status](https://ci.appveyor.com/api/projects/status/dtgpxv20ufnbaqeb?svg=true)](https://ci.appveyor.com/project/SergeyKulachenko/homework-04automated-testing-03-01)
---
+++ src/test/java/ru/netology/SelenideTest.java
@@ -0,0 +1,203 @@
+package ru.netology;
+
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.text.SimpleDateFormat;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTest {
+    @BeforeEach
+    void befor() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void getTrueInputValidForm() {
+        $("[placeholder=Город]").setValue("Краснодар");
+        long dateNowPlus4Days = System.currentTimeMillis() + 4 * 86400000;
+        SimpleDateFormat formatForDate = new SimpleDateFormat("dd.MM.yyyy");
+        String dateNowPlus4DaysStr = formatForDate.format(dateNowPlus4Days);
+        SelenideElement data = $("[class='calendar-input calendar-input_width_available calendar-input_theme_alfa-on-white']");
+        data.$("[value]").sendKeys("\b\b\b\b\b\b\b\b");
+        data.$("[placeholder]").setValue(dateNowPlus4DaysStr);
+        $("[data-test-id=name].input_type_text .input__control").setValue("Васильев Иван");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+79882223345");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $(withText("Встреча успешно забронирована на")).waitUntil(visible, 15000);
+    }
+
+    @Test
+    void errorExpectedWhenEmptyFieldCity() {
+        $("[placeholder=Город]").setValue("");
+        long dateNowPlus4Days = System.currentTimeMillis() + 4 * 86400000;
+        SimpleDateFormat formatForDate = new SimpleDateFormat("dd.MM.yyyy");
+        String dateNowPlus4DaysStr = formatForDate.format(dateNowPlus4Days);
+        SelenideElement data = $("[class='calendar-input calendar-input_width_available calendar-input_theme_alfa-on-white']");
+        data.$("[value]").sendKeys("\b\b\b\b\b\b\b\b");
+        data.$("[placeholder]").setValue(dateNowPlus4DaysStr);
+        $("[data-test-id=name].input_type_text .input__control").setValue("Васильев Иван");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+79882223345");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[class='form-field form-field_size_m form-field_theme_alfa-on-white'] .input__sub").shouldHave
+                (exactTextCaseSensitive("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void errorExpectedWhenInputIncorrectCity() {
+        $("[placeholder=Город]").setValue("Сочи");
+        long dateNowPlus4Days = System.currentTimeMillis() + 4 * 86400000;
+        SimpleDateFormat formatForDate = new SimpleDateFormat("dd.MM.yyyy");
+        String dateNowPlus4DaysStr = formatForDate.format(dateNowPlus4Days);
+        SelenideElement data = $("[class='calendar-input calendar-input_width_available calendar-input_theme_alfa-on-white']");
+        data.$("[value]").sendKeys("\b\b\b\b\b\b\b\b");
+        data.$("[placeholder]").setValue(dateNowPlus4DaysStr);
+        $("[data-test-id=name].input_type_text .input__control").setValue("Васильев Иван");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+79882223345");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[class='form-field form-field_size_m form-field_theme_alfa-on-white'] .input__sub").shouldHave
+                (exactTextCaseSensitive("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void errorExpectedWhenEmptyFieldDate() {
+        $("[placeholder=Город]").setValue("Краснодар");
+        SelenideElement data = $("[class='calendar-input calendar-input_width_available calendar-input_theme_alfa-on-white']");
+        data.$("[value]").sendKeys("\b\b\b\b\b\b\b\b");
+        $("[class='calendar-input calendar-input_width_available calendar-input_theme_alfa-on-white']").$("[placeholder]").setValue("");
+        $("[data-test-id=name].input_type_text .input__control").setValue("Васильев Иван");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+79882223345");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[data-test-id=date] .input__sub").shouldHave
+                (exactTextCaseSensitive("Неверно введена дата"));
+    }
+
+    @Test
+    void errorExpectedWhenInputIncorrectDate() {
+        $("[placeholder=Город]").setValue("Краснодар");
+        long dateNowPlus4Days = System.currentTimeMillis() + 2 * 86400000;
+        SimpleDateFormat formatForDate = new SimpleDateFormat("dd.MM.yyyy");
+        String dateNowPlus4DaysStr = formatForDate.format(dateNowPlus4Days);
+        SelenideElement data = $("[class='calendar-input calendar-input_width_available calendar-input_theme_alfa-on-white']");
+        data.$("[value]").sendKeys("\b\b\b\b\b\b\b\b");
+        data.$("[placeholder]").setValue(dateNowPlus4DaysStr);
+        $("[data-test-id=name].input_type_text .input__control").setValue("Васильев Иван");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+79882223345");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[data-test-id=date] .input__sub").shouldHave
+                (exactTextCaseSensitive("Заказ на выбранную дату невозможен"));
+    }
+
+    @Test
+    void errorExpectedWhenEmptyFieldName() {
+        $("[placeholder=Город]").setValue("Краснодар");
+        long dateNowPlus4Days = System.currentTimeMillis() + 4 * 86400000;
+        SimpleDateFormat formatForDate = new SimpleDateFormat("dd.MM.yyyy");
+        String dateNowPlus4DaysStr = formatForDate.format(dateNowPlus4Days);
+        SelenideElement data = $("[class='calendar-input calendar-input_width_available calendar-input_theme_alfa-on-white']");
+        data.$("[value]").sendKeys("\b\b\b\b\b\b\b\b");
+        data.$("[placeholder]").setValue(dateNowPlus4DaysStr);
+        $("[data-test-id=name].input_type_text .input__control").setValue("");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+79882223345");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[data-test-id=name] .input__sub").shouldHave
+                (exactTextCaseSensitive("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void errorExpectedWhenInputIncorrectNameNoRu() {
+        $("[placeholder=Город]").setValue("Краснодар");
+        long dateNowPlus4Days = System.currentTimeMillis() + 4 * 86400000;
+        SimpleDateFormat formatForDate = new SimpleDateFormat("dd.MM.yyyy");
+        String dateNowPlus4DaysStr = formatForDate.format(dateNowPlus4Days);
+        SelenideElement data = $("[class='calendar-input calendar-input_width_available calendar-input_theme_alfa-on-white']");
+        data.$("[value]").sendKeys("\b\b\b\b\b\b\b\b");
+        data.$("[placeholder]").setValue(dateNowPlus4DaysStr);
+        $("[data-test-id=name].input_type_text .input__control").setValue("Nik");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+79882223345");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[data-test-id=name] .input__sub").shouldHave
+                (exactTextCaseSensitive("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void errorExpectedWhenInputIncorrectNameNoAsPasport() {
+        $("[placeholder=Город]").setValue("Краснодар");
+        long dateNowPlus4Days = System.currentTimeMillis() + 4 * 86400000;
+        SimpleDateFormat formatForDate = new SimpleDateFormat("dd.MM.yyyy");
+        String dateNowPlus4DaysStr = formatForDate.format(dateNowPlus4Days);
+        SelenideElement data = $("[class='calendar-input calendar-input_width_available calendar-input_theme_alfa-on-white']");
+        data.$("[value]").sendKeys("\b\b\b\b\b\b\b\b");
+        data.$("[placeholder]").setValue(dateNowPlus4DaysStr);
+        $("[data-test-id=name].input_type_text .input__control").setValue("Сергей");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+79882223345");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[data-test-id=name] .input__sub").shouldHave
+                (exactTextCaseSensitive("Укажите точно как в паспорте"));
+    }
+
+    @Test
+    void errorExpectedWhenEmptyFieldTel() {
+        $("[placeholder=Город]").setValue("Краснодар");
+        long dateNowPlus4Days = System.currentTimeMillis() + 4 * 86400000;
+        SimpleDateFormat formatForDate = new SimpleDateFormat("dd.MM.yyyy");
+        String dateNowPlus4DaysStr = formatForDate.format(dateNowPlus4Days);
+        SelenideElement data = $("[class='calendar-input calendar-input_width_available calendar-input_theme_alfa-on-white']");
+        data.$("[value]").sendKeys("\b\b\b\b\b\b\b\b");
+        data.$("[placeholder]").setValue(dateNowPlus4DaysStr);
+        $("[data-test-id=name].input_type_text .input__control").setValue("Васильев Иван");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[data-test-id=phone] .input__sub").shouldHave
+                (exactTextCaseSensitive("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void errorExpectedWhenInputIncorrectTel() {
+        $("[placeholder=Город]").setValue("Краснодар");
+        long dateNowPlus4Days = System.currentTimeMillis() + 4 * 86400000;
+        SimpleDateFormat formatForDate = new SimpleDateFormat("dd.MM.yyyy");
+        String dateNowPlus4DaysStr = formatForDate.format(dateNowPlus4Days);
+        SelenideElement data = $("[class='calendar-input calendar-input_width_available calendar-input_theme_alfa-on-white']");
+        data.$("[value]").sendKeys("\b\b\b\b\b\b\b\b");
+        data.$("[placeholder]").setValue(dateNowPlus4DaysStr);
+        $("[data-test-id=name].input_type_text .input__control").setValue("Васильев Иван");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+7 9882253345");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[data-test-id=phone] .input__sub").shouldHave
+                (exactTextCaseSensitive("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+
+    @Test
+    void errorExpectedWhenUncheckedCheckbox() {
+        $("[placeholder=Город]").setValue("Краснодар");
+        long dateNowPlus4Days = System.currentTimeMillis() + 4 * 86400000;
+        SimpleDateFormat formatForDate = new SimpleDateFormat("dd.MM.yyyy");
+        String dateNowPlus4DaysStr = formatForDate.format(dateNowPlus4Days);
+        SelenideElement data = $("[class='calendar-input calendar-input_width_available calendar-input_theme_alfa-on-white']");
+        data.$("[value]").sendKeys("\b\b\b\b\b\b\b\b");
+        data.$("[placeholder]").setValue(dateNowPlus4DaysStr);
+        $("[data-test-id=name].input_type_text .input__control").setValue("Васильев Иван");
+        $("[data-test-id=phone]").$("[name=phone]").setValue("+79882253345");
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[class='checkbox checkbox_size_m checkbox_theme_alfa-on-white input_invalid'] .checkbox__text").shouldHave
+                (exactTextCaseSensitive("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+    }
+
+}
