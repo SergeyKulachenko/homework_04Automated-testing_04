1556259078d68189f197aa98530b46319188b04f
Sergey K <89184083452.mail.ru>
2020-10-18 00:35:38 +0300

    настройка по дате

--- src/test/java/ru/netology/SelenideTest.java
+++ src/test/java/ru/netology/SelenideTest.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import javax.annotation.processing.Completion;
-import java.text.SimpleDateFormat;
+import javax.xml.crypto.Data;
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
@@ -22,45 +22,19 @@ public class SelenideTest {
     }
 
     @Test
-    void getTrueInputValidFormTask2V1() {
+    void getTrueInputValidFormTask2() {
         $("[placeholder=Город]").setValue("Кр");
-        $$("[class='menu menu_size_m menu_group-view_default menu_mode_radio-check menu_theme_alfa-on-white input__menu'] .menu-item__control")
-                .find(exactText("Красноярск")).click();
-        LocalDate localDate = LocalDate.now();
-        DateTimeFormatter fOut = DateTimeFormatter.ofPattern("dd/MM/uuuu");
-        String inputDate = localDate.plusDays(4).format(fOut);
-        SelenideElement data = $("[class='calendar-input calendar-input_width_available calendar-input_theme_alfa-on-white']");
-        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
-        data.$("[placeholder]").setValue(inputDate);
-        $("[data-test-id=name].input_type_text .input__control").setValue("Васильев Иван");
-        $("[data-test-id=phone]").$("[name=phone]").setValue("+79882223345");
-        $("[class=checkbox__box]").click();
-        $$("[class=button__text]").find(exactText("Забронировать")).click();
-        $(withText("Встреча успешно забронирована на")).waitUntil(visible, 15000);
-    }
-
-    @Test
-    void getTrueInputValidFormTask2V2() {
-        $("[placeholder=Город]").setValue("Кр");
-        $$("[class='menu menu_size_m menu_group-view_default menu_mode_radio-check menu_theme_alfa-on-white input__menu'] .menu-item__control")
+        $$("[class=popup__content] .menu-item__control")
                 .last().click();
-        LocalDate localDate = LocalDate.now();
+        int localDate = LocalDate.now().getMonthValue();
+        int localDatePlus = LocalDate.now().plusDays(55).getMonthValue();
+        String datePlus = String.valueOf(LocalDate.now().plusDays(55).getDayOfMonth());
+        $("[class=input__icon]").click();
+        for (; (localDatePlus - localDate) > 0; localDate++) {
+            $("[class=calendar__title]").$("[class='calendar__arrow calendar__arrow_direction_right']").click();
+        }
+        $$("td.calendar__day").find(exactText(datePlus)).click();
 
-//        $("[class=input__icon]").click();
-//        localDate.getDayOfMonth();
-//        $("[class=calendar-input__calendar-wrapper]").$("[data-day=1603314000000]").click();
-//        String a=$("[class=calendar-input__calendar-wrapper]").getValue();
-//        String aa=$("[class=calendar-input__calendar-wrapper]").getText();
-//        String s=$("[class=popup__container]").getText();
-//        String ds=$("[class=popup__container]").getSelectedText();
-//        String ww=$("[class='calendar-input__calendar-wrapper']").getSelectedValue();
-//        $("[data-step='1']").click();
-
-        DateTimeFormatter fOut = DateTimeFormatter.ofPattern("dd/MM/uuuu");
-        String inputDate = localDate.plusDays(4).format(fOut);
-        SelenideElement data = $("[class='calendar-input calendar-input_width_available calendar-input_theme_alfa-on-white']");
-        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
-        data.$("[placeholder]").setValue(inputDate);
         $("[data-test-id=name].input_type_text .input__control").setValue("Васильев Иван");
         $("[data-test-id=phone]").$("[name=phone]").setValue("+79882223345");
         $("[class=checkbox__box]").click();
